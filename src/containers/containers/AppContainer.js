2abee423704db5196fb72dc141c1ef8dd5e9bd17
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2020-01-23 02:00:34 +0530

    added-init-dar-knight

--- src/containers/containers/AppContainer.js
+++ src/containers/containers/AppContainer.js
@@ -1,62 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import {
   actions as userActions,
   selectors as userSelectors
-} from '../../redux/user'
+} from '../../redux/user';
 
 import {
   actions as modelActions,
   selectors as modelSelectors
-} from '../../redux/models'
+} from '../../redux/models';
 
-import { bindSelectors } from '../utils/redux-utils'
+import { bindSelectors } from '../utils/redux-utils';
 
-const boundUserSelectors = bindSelectors(userSelectors, 'user')
-const boundModelSelectors = bindSelectors(modelSelectors, 'models')
+const boundUserSelectors = bindSelectors(userSelectors, 'user');
+const boundModelSelectors = bindSelectors(modelSelectors, 'models');
 
 class AppContainer extends React.Component {
   constructor(props) {
     super(props);
     // Move fetch() to global so errors, presently just 401s, can be intercepted
-    const fetch = global.fetch  
+    const fetch = global.fetch;
 
     global.fetch = function(url, options) {
       return fetch(url, options).then(response => {
         if (response.status === 401) {
-          props.getAuthenticationStatus()
+          props.getAuthenticationStatus();
         }
-        return response
-      })
-    }
+        return response;
+      });
+    };
   }
 
   componentDidMount() {
     if (!this.props.currentUser) {
-      this.props.getAuthenticationStatus()
+      this.props.getAuthenticationStatus();
     }
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.isAuthenticated && !this.props.isAuthenticated) {
-      this.props.localLogout()
+      this.props.localLogout();
     }
 
     // Once logged in, load app configuration model
     if (!prevProps.isAuthenticated && this.props.isAuthenticated) {
-      const appId = `/applications/${process.env.REACT_APP_ID}`
-      this.props.loadModel(appId)
+      const appId = `/applications/${process.env.REACT_APP_ID}`;
+      this.props.loadModel(appId);
     }
   }
 
   render() {
     if (this.props.appModelPending) {
-      return null
+      return null;
     }
-    return this.props.render(this.props)
+    return this.props.render(this.props);
   }
 }
 
@@ -68,20 +68,21 @@ AppContainer.propTypes = {
   render: PropTypes.func.isRequired,
   submitLogout: PropTypes.func.isRequired,
   localLogout: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = (state, ownProps) => {
-  const appId = `/applications/${process.env.REACT_APP_ID}`
+  const appId = `/applications/${process.env.REACT_APP_ID}`;
   return {
-    isAuthenticated: boundUserSelectors.isCurrentUserAuthenticated(state) || false,
+    isAuthenticated:
+      boundUserSelectors.isCurrentUserAuthenticated(state) || true,
     currentUser: boundUserSelectors.currentUser(state),
     profile: boundUserSelectors.profile(state),
     appModelPending: boundModelSelectors.isModelFetchPending(state, appId),
     appModel: boundModelSelectors.getModel(state, appId),
 
     ...ownProps
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
@@ -90,7 +91,7 @@ const mapDispatchToProps = dispatch =>
       localLogout: userActions.localLogout,
       getAuthenticationStatus: userActions.getAuthenticationStatus,
       loadModel: modelActions.loadModel,
-      loadSchema: modelActions.loadSchema,
+      loadSchema: modelActions.loadSchema
     },
     dispatch
   );
